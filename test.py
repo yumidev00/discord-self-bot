d19844c28f7646eeba5e32c3cb961cf9f252bdc6
yumidev00 <yumidev00@gmail.com>
2024-01-19 13:37:39 +0100

    Update Fri Jan 19 01:37:39 PM CET 2024

---
+++ test.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!', self_bot=True)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+    await bot.change_presence(activity=discord.Game(name='Hello World'), status=discord.Status.dnd)
+
+@bot.event
+async def on_message(message):
+    # Ignore messages from the bot itself
+    if message.author == bot.user:
+        # send message to the channel
+        if message.content.startswith('!chat'):
+            await message.channel.send('REPLY HERE')
+
+bot.run('TOKEN HERE')
+
+ 
